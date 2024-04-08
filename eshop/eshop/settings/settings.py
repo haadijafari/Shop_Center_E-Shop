9e845176ce8448d95c8c4a41e418f8cada5872c7
Hadi Jafari <haadijafari2003@gmail.com>
2024-04-08 16:38:50 +0330

    [ADD] Create Index App
    [+] Modify Index URLs, template and view

--- eshop/eshop/settings/settings.py
+++ eshop/eshop/settings/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'taggit',
 
     # apps
+    'apps.index',
     'apps.product',
 ]
 
--- eshop/eshop/urls.py
+++ eshop/eshop/urls.py
@@ -23,11 +23,12 @@ from eshop.settings.settings import DEBUG
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('apps.index.urls')),
 ]
 
 if DEBUG:
     urlpatterns += [
-        path("__debug__/", include("debug_toolbar.urls"))
+        path("__debug__/", include("debug_toolbar.urls")),
     ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
