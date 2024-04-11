cce7c37ccfdc3b6e18e2b3235fc9587e09336793
Hadi Jafari <haadijafari2003@gmail.com>
2024-04-11 23:21:08 +0330

    [FIX] Product Details
    [+] Fetch Title, Price from database
    [+] Humanize Price
    [+] Fix recommended products image

--- eshop/apps/product/urls.py
+++ eshop/apps/product/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'product'
+
 urlpatterns = [
-    path('product-list/', views.product_list_view,name='product_list'),
-    path('product-details/', views.product_details_view, name='product_details'),
+    path('product-list/', views.product_list_view, name='product_list'),
+    path('product-details/<int:pid>/<slug:slug>', views.product_details_view, name='product_details'),
 ]
--- eshop/apps/product/views.py
+++ eshop/apps/product/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
 
+from apps.product.models import Product
 
-# Create your views here.
+
 def product_list_view(request):
-    return render(request, 'product/product_list.html')
+    context = {
+        'products': Product.objects.all()
+    }
+    return render(request, 'product/product_list.html', context)
 
 
-def product_details_view(request):
-    return render(request, 'product/product_details.html')
+def product_details_view(request, pid, slug):
+    context = {
+        'product': Product.objects.get(id=pid, slug=slug)
+    }
+    print(context)
+    return render(request, 'product/product_details.html', context)
