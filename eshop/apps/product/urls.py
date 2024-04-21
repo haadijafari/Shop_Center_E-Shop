e5453b2cc3452b2f224bd17b992fe2314bd7f8d6
Hadi Jafari <haadijafari2003@gmail.com>
2024-04-21 16:46:42 +0330

    [ADD] Products Class Base View
    [+] Add product list class base view
    [+] Add product details class base view

--- eshop/apps/product/urls.py
+++ eshop/apps/product/urls.py
@@ -4,6 +4,8 @@ from . import views
 app_name = 'product'
 
 urlpatterns = [
-    path('', views.product_list_view, name='product_list'),
-    path('details/<int:pid>/<slug:slug>', views.product_details_view, name='product_details'),
+    # path('', views.product_list_view, name='product_list'),
+    path('', views.ProductListView.as_view(), name='product_list'),
+    # path('details/<int:pid>/<slug:slug>', views.product_details_view, name='product_details'),
+    path('details/<int:pid>/<slug:slug>', views.ProductDetailsView.as_view(), name='product_details'),
 ]
--- eshop/apps/product/views.py
+++ eshop/apps/product/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
 
 from apps.product.models import Product
+from django.views.generic import TemplateView, ListView
+
+
+class ProductListView(ListView):
+    template_name = 'product/product_list.html'
+    model = Product
+    context_object_name = 'products'
+
+    def get_queryset(self):
+        base_query = super(ProductListView, self).get_queryset()
+        return base_query.filter(is_active=True)
 
 
 def product_list_view(request):
@@ -8,6 +19,15 @@ def product_list_view(request):
         'products': Product.objects.all()
     }
     return render(request, 'product/product_list.html', context)
+
+
+class ProductDetailsView(TemplateView):
+    template_name = 'product/product_details.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(ProductDetailsView, self).get_context_data()
+        context['product'] = Product.objects.get(id=kwargs.get('pid'), slug=kwargs.get('slug'))
+        return context
 
 
 def product_details_view(request, pid, slug):
