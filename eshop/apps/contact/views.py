27f16223d718a95e9084c65c0d47d44ab201e13e
Hadi Jafari <haadijafari2003@gmail.com>
2024-04-22 20:19:54 +0330

    [FIX] Contact Circular Error

--- eshop/apps/contact/views.py
+++ eshop/apps/contact/views.py
@@ -2,14 +2,13 @@ from django.shortcuts import render, redirect
 
 from apps.contact.forms import ContactModelForm
 from django.urls import reverse
-from django.views import View
 from django.views.generic import FormView
 
 
 class ContactView(FormView):
     template_name = 'contact/contact.html'
     form_class = ContactModelForm
-    success_url = reverse('contact:contact')
+    success_url = '/contact/'
 
     def form_valid(self, form):
         form.save()
