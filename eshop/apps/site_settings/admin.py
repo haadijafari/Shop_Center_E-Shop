ea570bcbe793698bcea08f340079ab60cdc9ab42
Hadi Jafari <haadijafari2003@gmail.com>
2024-07-16 06:00:24 +0330

    [FIX] Site Settings Model
    [+] Fix Site Settings Model Logic

--- eshop/apps/site_settings/admin.py
+++ eshop/apps/site_settings/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import AboutUs, SiteSetting, FooterLinkBox, FooterLink, Slider
 
-# Register your models here.
+
+@admin.register(AboutUs)
+class ProductAdmin(admin.ModelAdmin):
+    list_filter = ['is_main_setting']
+    list_display = ['__str__', 'phone', 'email', 'is_main_setting']
+    list_editable = ['is_main_setting']
+
+
+@admin.register(SiteSetting)
+class ProductAdmin(admin.ModelAdmin):
+    list_filter = ['is_main_setting']
+    list_display = ['__str__', 'is_main_setting']
+    list_editable = ['is_main_setting']
+
+
+admin.site.register(FooterLinkBox)
+admin.site.register(FooterLink)
+admin.site.register(Slider)
--- eshop/apps/site_settings/models.py
+++ eshop/apps/site_settings/models.py
@@ -4,12 +4,7 @@ from django.utils.translation import gettext_lazy as _
 
 class SiteSetting(models.Model):
     site_title = models.CharField(_('عنوان سایت'), max_length=128)
-    address = models.CharField(_('آدرس'), max_length=512, null=True, blank=True)
-    phone = models.CharField(_('شماره تماس'), max_length=100, null=True, blank=True)
-    fax = models.CharField(_('فکس'), max_length=128, null=True, blank=True)
-    email = models.CharField(_('ایمیل'), max_length=128, null=True, blank=True)
     copy_right = models.TextField(_('متن کپی رایت سایت'))
-    about_us_text = models.TextField(_('متن درباره ما سایت'))
     logo = models.ImageField(_('لوگو'), upload_to='images/settings/logo')
     is_main_setting = models.BooleanField(_('تنظیمات اصلی'), default=False)
 
@@ -21,8 +16,26 @@ class SiteSetting(models.Model):
         return self.site_name
 
 
+class AboutUs(models.Model):
+    about_us_title = models.CharField(_('عنوان'), max_length=128)
+    address = models.CharField(_('آدرس'), max_length=512, null=True, blank=True)
+    phone = models.CharField(_('شماره تماس'), max_length=100, null=True, blank=True)
+    fax = models.CharField(_('فکس'), max_length=128, null=True, blank=True)
+    email = models.CharField(_('ایمیل'), max_length=128, null=True, blank=True)
+    about_us_text = models.TextField(_('متن درباره ما سایت'))
+    is_main_setting = models.BooleanField(_('تنظیمات اصلی'), default=False)
+
+    class Meta:
+        verbose_name = _('درباره ما')
+        verbose_name_plural = _('مشخصات درباره ما')
+
+    def __str__(self):
+        return self.about_us_title
+
+
 class FooterLinkBox(models.Model):
     title = models.CharField(_('عنوان'), max_length=200)
+    is_active = models.BooleanField(_('فعال / غیرفعال'), default=True)
 
     class Meta:
         verbose_name = _('دسته بندی لینک های فوتر')
@@ -35,7 +48,8 @@ class FooterLinkBox(models.Model):
 class FooterLink(models.Model):
     title = models.CharField(_('عنوان'), max_length=128)
     url = models.URLField(_('لینک'), max_length=512)
-    footer_link_box = models.ForeignKey(to=FooterLinkBox, on_delete=models.CASCADE, verbose_name=_('دسته بندی') )
+    footer_link_box = models.ForeignKey(to=FooterLinkBox, on_delete=models.CASCADE, verbose_name=_('دسته بندی'))
+    is_active = models.BooleanField(_('فعال / غیرفعال'), default=True)
 
     class Meta:
         verbose_name = _('لینک فوتر')
@@ -59,4 +73,3 @@ class Slider(models.Model):
 
     def __str__(self):
         return self.title
-
