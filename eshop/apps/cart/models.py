a20942c7f7683521ed75f1940f3e87aa9aeca3fc
Hadi Jafari <haadijafari2003@gmail.com>
2024-07-19 12:57:10 +0330

    [ADD] Cart Model
    [+] Create Cart Model and Admin Setting

--- eshop/apps/cart/models.py
+++ eshop/apps/cart/models.py
@@ -1,3 +1,38 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from apps.product.models import Product
+
+User = get_user_model()
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name=_('کاربر'))
+    is_paid = models.BooleanField(_('وضعیت پرداخت'), default=False)
+    created_date = models.DateTimeField(_('تاریخ ایجاد'), auto_now_add=True)
+    paid_date = models.DateTimeField(_('تاریخ پرداخت'), null=True, blank=True)
+
+    def __str__(self):
+        return str(self.user)
+
+    class Meta:
+        verbose_name = _('سبد خرید')
+        verbose_name_plural = _('سبدهای خرید کاربران')
+
+
+class CartDetail(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, verbose_name=_('سبد'))
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, verbose_name=_('محصول'))
+    final_price = models.IntegerField(_('قیمت حساب شده'), null=True, blank=True)
+    count = models.IntegerField(_('تعداد'), default=1, null=False, blank=False)
+
+    def get_total_price(self):
+        return self.count * self.product.price
+
+    def __str__(self):
+        return f'{str(self.cart)} | {self.product.title}'
+
+    class Meta:
+        verbose_name = _('آیتم سبد خرید')
+        verbose_name_plural = _('لیست محتویات سبدهای خرید')
